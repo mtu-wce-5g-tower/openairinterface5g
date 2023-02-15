dfki-in-icc/5G-PRACH-Smart-Jammer

intermediate commit

openair1/PHY/NR_UE_TRANSPORT/csi_rx.c
@@ -55,30 +55,30 @@ void nr_det_A_MF_2x2(int32_t *a_mf_00,
 
   int16_t nr_conjug2[8]__attribute__((aligned(16))) = {1,-1,1,-1,1,-1,1,-1} ;
 
-  __m128i ad_re_128, bc_re_128, det_re_128;
+  simde__m128i ad_re_128, bc_re_128, det_re_128;
 
-  __m128i *a_mf_00_128 = (__m128i *)a_mf_00;
-  __m128i *a_mf_01_128 = (__m128i *)a_mf_01;
-  __m128i *a_mf_10_128 = (__m128i *)a_mf_10;
-  __m128i *a_mf_11_128 = (__m128i *)a_mf_11;
-  __m128i *det_fin_128 = (__m128i *)det_fin;
+  simde__m128i *a_mf_00_128 = (simde__m128i *)a_mf_00;
+  simde__m128i *a_mf_01_128 = (simde__m128i *)a_mf_01;
+  simde__m128i *a_mf_10_128 = (simde__m128i *)a_mf_10;
+  simde__m128i *a_mf_11_128 = (simde__m128i *)a_mf_11;
+  simde__m128i *det_fin_128 = (simde__m128i *)det_fin;
 
   for (int rb = 0; rb<3*nb_rb; rb++) {
 
     //complex multiplication (I_a+jQ_a)(I_d+jQ_d) = (I_aI_d - Q_aQ_d) + j(Q_aI_d + I_aQ_d)
     //The imag part is often zero, we compute only the real part
-    ad_re_128 = _mm_sign_epi16(a_mf_00_128[0],*(__m128i*)&nr_conjug2[0]);
-    ad_re_128 = _mm_madd_epi16(ad_re_128,a_mf_11_128[0]); //Re: I_a0*I_d0 - Q_a1*Q_d1
+    ad_re_128 = simde_mm_sign_epi16(a_mf_00_128[0],*(simde__m128i*)&nr_conjug2[0]);
+    ad_re_128 = simde_mm_madd_epi16(ad_re_128,a_mf_11_128[0]); //Re: I_a0*I_d0 - Q_a1*Q_d1
 
     //complex multiplication (I_b+jQ_b)(I_c+jQ_c) = (I_bI_c - Q_bQ_c) + j(Q_bI_c + I_bQ_c)
     //The imag part is often zero, we compute only the real part
-    bc_re_128 = _mm_sign_epi16(a_mf_01_128[0],*(__m128i*)&nr_conjug2[0]);
-    bc_re_128 = _mm_madd_epi16(bc_re_128,a_mf_10_128[0]); //Re: I_b0*I_c0 - Q_b1*Q_c1
+    bc_re_128 = simde_mm_sign_epi16(a_mf_01_128[0],*(simde__m128i*)&nr_conjug2[0]);
+    bc_re_128 = simde_mm_madd_epi16(bc_re_128,a_mf_10_128[0]); //Re: I_b0*I_c0 - Q_b1*Q_c1
 
-    det_re_128 = _mm_sub_epi32(ad_re_128, bc_re_128);
+    det_re_128 = simde_mm_sub_epi32(ad_re_128, bc_re_128);
 
     //det in Q30 format
-    det_fin_128[0] = _mm_abs_epi32(det_re_128);
+    det_fin_128[0] = simde_mm_abs_epi32(det_re_128);
 
     det_fin_128+=1;
     a_mf_00_128+=1;
@@ -93,10 +93,10 @@ void nr_det_A_MF_2x2(int32_t *a_mf_00,
 void nr_squared_matrix_element(int32_t *a,
                                int32_t *a_sq,
                                const unsigned short nb_rb) {
-  __m128i *a_128 = (__m128i *)a;
-  __m128i *a_sq_128 = (__m128i *)a_sq;
+  simde__m128i *a_128 = (simde__m128i *)a;
+  simde__m128i *a_sq_128 = (simde__m128i *)a_sq;
   for (int rb=0; rb<3*nb_rb; rb++) {
-    a_sq_128[0] = _mm_madd_epi16(a_128[0], a_128[0]);
+    a_sq_128[0] = simde_mm_madd_epi16(a_128[0], a_128[0]);
     a_sq_128+=1;
     a_128+=1;
   }
@@ -110,15 +110,15 @@ void nr_numer_2x2(int32_t *a_00_sq,
                   int32_t *a_11_sq,
                   int32_t *num_fin,
                   const unsigned short nb_rb) {
-  __m128i *a_00_sq_128 = (__m128i *)a_00_sq;
-  __m128i *a_01_sq_128 = (__m128i *)a_01_sq;
-  __m128i *a_10_sq_128 = (__m128i *)a_10_sq;
-  __m128i *a_11_sq_128 = (__m128i *)a_11_sq;
-  __m128i *num_fin_128 = (__m128i *)num_fin;
+  simde__m128i *a_00_sq_128 = (simde__m128i *)a_00_sq;
+  simde__m128i *a_01_sq_128 = (simde__m128i *)a_01_sq;
+  simde__m128i *a_10_sq_128 = (simde__m128i *)a_10_sq;
+  simde__m128i *a_11_sq_128 = (simde__m128i *)a_11_sq;
+  simde__m128i *num_fin_128 = (simde__m128i *)num_fin;
   for (int rb=0; rb<3*nb_rb; rb++) {
-    __m128i sq_a_plus_sq_d_128 = _mm_add_epi32(a_00_sq_128[0], a_11_sq_128[0]);
-    __m128i sq_b_plus_sq_c_128 = _mm_add_epi32(a_01_sq_128[0], a_10_sq_128[0]);
-    num_fin_128[0] = _mm_add_epi32(sq_a_plus_sq_d_128, sq_b_plus_sq_c_128);
+    simde__m128i sq_a_plus_sq_d_128 = simde_mm_add_epi32(a_00_sq_128[0], a_11_sq_128[0]);
+    simde__m128i sq_b_plus_sq_c_128 = simde_mm_add_epi32(a_01_sq_128[0], a_10_sq_128[0]);
+    num_fin_128[0] = simde_mm_add_epi32(sq_a_plus_sq_d_128, sq_b_plus_sq_c_128);
     num_fin_128+=1;
     a_00_sq_128+=1;
     a_01_sq_128+=1;
@@ -511,8 +511,8 @@ int nr_csi_rs_ri_estimation(const PHY_VARS_NR_UE *ue,
 
             // construct Hh x H elements
             if(ant_rx_conjch == ant_rx_ch) {
-              nr_a_sum_b((__m128i *)&csi_rs_estimated_A_MF[port_tx_conjch][port_tx_ch][k],
-                         (__m128i *)&csi_rs_estimated_conjch_ch[ant_rx_conjch][port_tx_conjch][ant_rx_ch][port_tx_ch][k],
+              nr_a_sum_b((simde__m128i *)&csi_rs_estimated_A_MF[port_tx_conjch][port_tx_ch][k],
+                         (simde__m128i *)&csi_rs_estimated_conjch_ch[ant_rx_conjch][port_tx_conjch][ant_rx_ch][port_tx_ch][k],
                          1);
             }
           }

mtu-wce-5g-tower/openairinterface5g

openair1/PHY/NR_UE_TRANSPORT/csi_rx.c
@@ -58,30 +58,30 @@ void nr_det_A_MF_2x2(int32_t *a_mf_00,
 
   int16_t nr_conjug2[8]__attribute__((aligned(16))) = {1,-1,1,-1,1,-1,1,-1} ;
 
-  __m128i ad_re_128, bc_re_128, det_re_128;
+  simde__m128i ad_re_128, bc_re_128, det_re_128;
 
-  __m128i *a_mf_00_128 = (__m128i *)a_mf_00;
-  __m128i *a_mf_01_128 = (__m128i *)a_mf_01;
-  __m128i *a_mf_10_128 = (__m128i *)a_mf_10;
-  __m128i *a_mf_11_128 = (__m128i *)a_mf_11;
-  __m128i *det_fin_128 = (__m128i *)det_fin;
+  simde__m128i *a_mf_00_128 = (simde__m128i *)a_mf_00;
+  simde__m128i *a_mf_01_128 = (simde__m128i *)a_mf_01;
+  simde__m128i *a_mf_10_128 = (simde__m128i *)a_mf_10;
+  simde__m128i *a_mf_11_128 = (simde__m128i *)a_mf_11;
+  simde__m128i *det_fin_128 = (simde__m128i *)det_fin;
 
   for (int rb = 0; rb<3*nb_rb; rb++) {
 
     //complex multiplication (I_a+jQ_a)(I_d+jQ_d) = (I_aI_d - Q_aQ_d) + j(Q_aI_d + I_aQ_d)
     //The imag part is often zero, we compute only the real part
-    ad_re_128 = _mm_sign_epi16(a_mf_00_128[0],*(__m128i*)&nr_conjug2[0]);
-    ad_re_128 = _mm_madd_epi16(ad_re_128,a_mf_11_128[0]); //Re: I_a0*I_d0 - Q_a1*Q_d1
+    ad_re_128 = simde_mm_sign_epi16(a_mf_00_128[0],*(simde__m128i*)&nr_conjug2[0]);
+    ad_re_128 = simde_mm_madd_epi16(ad_re_128,a_mf_11_128[0]); //Re: I_a0*I_d0 - Q_a1*Q_d1
 
     //complex multiplication (I_b+jQ_b)(I_c+jQ_c) = (I_bI_c - Q_bQ_c) + j(Q_bI_c + I_bQ_c)
     //The imag part is often zero, we compute only the real part
-    bc_re_128 = _mm_sign_epi16(a_mf_01_128[0],*(__m128i*)&nr_conjug2[0]);
-    bc_re_128 = _mm_madd_epi16(bc_re_128,a_mf_10_128[0]); //Re: I_b0*I_c0 - Q_b1*Q_c1
+    bc_re_128 = simde_mm_sign_epi16(a_mf_01_128[0],*(simde__m128i*)&nr_conjug2[0]);
+    bc_re_128 = simde_mm_madd_epi16(bc_re_128,a_mf_10_128[0]); //Re: I_b0*I_c0 - Q_b1*Q_c1
 
-    det_re_128 = _mm_sub_epi32(ad_re_128, bc_re_128);
+    det_re_128 = simde_mm_sub_epi32(ad_re_128, bc_re_128);
 
     //det in Q30 format
-    det_fin_128[0] = _mm_abs_epi32(det_re_128);
+    det_fin_128[0] = simde_mm_abs_epi32(det_re_128);
 
     det_fin_128+=1;
     a_mf_00_128+=1;
@@ -96,10 +96,10 @@ void nr_det_A_MF_2x2(int32_t *a_mf_00,
 void nr_squared_matrix_element(int32_t *a,
                                int32_t *a_sq,
                                const unsigned short nb_rb) {
-  __m128i *a_128 = (__m128i *)a;
-  __m128i *a_sq_128 = (__m128i *)a_sq;
+  simde__m128i *a_128 = (simde__m128i *)a;
+  simde__m128i *a_sq_128 = (simde__m128i *)a_sq;
   for (int rb=0; rb<3*nb_rb; rb++) {
-    a_sq_128[0] = _mm_madd_epi16(a_128[0], a_128[0]);
+    a_sq_128[0] = simde_mm_madd_epi16(a_128[0], a_128[0]);
     a_sq_128+=1;
     a_128+=1;
   }
@@ -113,15 +113,15 @@ void nr_numer_2x2(int32_t *a_00_sq,
                   int32_t *a_11_sq,
                   int32_t *num_fin,
                   const unsigned short nb_rb) {
-  __m128i *a_00_sq_128 = (__m128i *)a_00_sq;
-  __m128i *a_01_sq_128 = (__m128i *)a_01_sq;
-  __m128i *a_10_sq_128 = (__m128i *)a_10_sq;
-  __m128i *a_11_sq_128 = (__m128i *)a_11_sq;
-  __m128i *num_fin_128 = (__m128i *)num_fin;
+  simde__m128i *a_00_sq_128 = (simde__m128i *)a_00_sq;
+  simde__m128i *a_01_sq_128 = (simde__m128i *)a_01_sq;
+  simde__m128i *a_10_sq_128 = (simde__m128i *)a_10_sq;
+  simde__m128i *a_11_sq_128 = (simde__m128i *)a_11_sq;
+  simde__m128i *num_fin_128 = (simde__m128i *)num_fin;
   for (int rb=0; rb<3*nb_rb; rb++) {
-    __m128i sq_a_plus_sq_d_128 = _mm_add_epi32(a_00_sq_128[0], a_11_sq_128[0]);
-    __m128i sq_b_plus_sq_c_128 = _mm_add_epi32(a_01_sq_128[0], a_10_sq_128[0]);
-    num_fin_128[0] = _mm_add_epi32(sq_a_plus_sq_d_128, sq_b_plus_sq_c_128);
+    simde__m128i sq_a_plus_sq_d_128 = simde_mm_add_epi32(a_00_sq_128[0], a_11_sq_128[0]);
+    simde__m128i sq_b_plus_sq_c_128 = simde_mm_add_epi32(a_01_sq_128[0], a_10_sq_128[0]);
+    num_fin_128[0] = simde_mm_add_epi32(sq_a_plus_sq_d_128, sq_b_plus_sq_c_128);
     num_fin_128+=1;
     a_00_sq_128+=1;
     a_01_sq_128+=1;
@@ -518,8 +518,9 @@ int nr_csi_rs_ri_estimation(const PHY_VARS_NR_UE *ue,
 
             // construct Hh x H elements
             if(ant_rx_conjch == ant_rx_ch) {
-              nr_a_sum_b((__m128i *)&csi_rs_estimated_A_MF[port_tx_conjch][port_tx_ch][k_offset],
-                         (__m128i *)&csi_rs_estimated_conjch_ch[ant_rx_conjch][port_tx_conjch][ant_rx_ch][port_tx_ch][k_offset],
+
+              nr_a_sum_b((simde__m128i *)&csi_rs_estimated_A_MF[port_tx_conjch][port_tx_ch][k_offset],
+                         (simde__m128i *)&csi_rs_estimated_conjch_ch[ant_rx_conjch][port_tx_conjch][ant_rx_ch][port_tx_ch][k_offset],
                          1);
             }
           }