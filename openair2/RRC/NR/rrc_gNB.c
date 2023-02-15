73c6cdb37eb853353dd6e5eb8b3ac6add1bca0e1
mir <mikel.irazabal@openairinterface.com>
2023-02-15 18:50:06 +0100

    Directory OSA removed, still need to merge things

--- openair2/RRC/NR/rrc_gNB.c
+++ openair2/RRC/NR/rrc_gNB.c
@@ -80,7 +80,8 @@
 #include "OCG.h"
 #include "OCG_extern.h"
 
-#include "UTIL/OSA/osa_defs.h"
+//#include "UTIL/OSA/osa_defs.h"
+#include "openair3/SECU/secu_defs.h"
 
 #include "rrc_eNB_S1AP.h"
 #include "rrc_gNB_NGAP.h"
